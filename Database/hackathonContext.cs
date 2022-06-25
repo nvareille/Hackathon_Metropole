c04d22d7a4cbf800a33872977e9a20b428495848
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2022-06-25 12:15:36 +0200

    fix mysql

--- Database/hackathonContext.cs
+++ Database/hackathonContext.cs
@@ -30,8 +30,11 @@ namespace Database
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+#if DEBUG
                 optionsBuilder.UseMySql("server=localhost;port=3306;user=root;database=hackathon", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.36-mysql"));
+#else
+                optionsBuilder.UseMySql("server=localhost;port=3307;user=root;database=hackathon", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.36-mysql"));
+#endif
             }
         }
 
