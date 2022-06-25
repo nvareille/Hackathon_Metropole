3a024d98319b797e40116f958b1a83df2504d07d
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2022-06-25 14:08:17 +0200

    Ajout des resources

---
+++ Web/HMetropole/Server/Controllers/ResourceController.cs
@@ -0,0 +1,40 @@
+﻿using API.Siret;
+using Database;
+using HMetropole.Shared.Models;
+using HMetropole.Shared.Payloads;
+using HMetropole.Shared.Results;
+using Microsoft.AspNetCore.Mvc;
+using Entreprise = HMetropole.Shared.Results.Entreprise;
+using Prestataire = HMetropole.Shared.Models.Prestataire;
+using Projet = HMetropole.Shared.Models.Projet;
+using Theme = HMetropole.Shared.Models.Theme;
+
+namespace HMetropole.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceController : ControllerBase
+    {
+        private Wrapper Wrapper;
+        private hackathonContext Context;
+
+        public ResourceController(Wrapper wrapper, hackathonContext context)
+        {
+            Wrapper = wrapper;
+            Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Resource> Index()
+        {
+            
+            return (Context.Ressources.Select(i => new Resource
+            {
+                Id = i.Id,
+                Name = i.Nom,
+                URL = i.Url,
+                SousTheme = i.Soustheme
+            }));
+        }
+    }
+}
---
+++ Web/HMetropole/Shared/Results/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMetropole.Shared.Models;
+
+namespace HMetropole.Shared.Results
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public int SousTheme { get; set; }
+    }
+}
